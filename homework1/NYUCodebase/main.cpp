c38745d3a618cf4866ec8465ed5a46a99da9d8fe
ddy214 <ddy214@nyu.edu>
2016-09-27 03:57:20 -0400

    finish assignment 1

--- homework1/NYUCodebase/main.cpp
+++ homework1/NYUCodebase/main.cpp
@@ -21,7 +21,7 @@ GLuint LoadTexture(const char *image_path) {
     GLuint textureID;
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGR, surface->w, surface->h, 0, GL_RGBA,
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_BGRA,
                  GL_UNSIGNED_BYTE, surface->pixels);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -44,12 +44,16 @@ int main(int argc, char *argv[])
     glViewport(0, 0, 640, 360);
     ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
     
-    GLuint shipTexture = LoadTexture("/Users/derekyu/projects/Game-Programming-CS-3113/homework1/NYUCodebase/playerShip1_blue.png");
+    GLuint shipTexture = LoadTexture("playerShip1_blue.png");
+    GLuint shipTexture2 = LoadTexture("playerShip2_red.png");
+    GLuint shipTexture3 = LoadTexture("playerShip3_orange.png");
     
     
     Matrix projectionMatrix;
     Matrix modelMatrix;
     Matrix viewMatrix;
+    
+    float lastFrameTicks = 0.0f;
     
     projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
     glUseProgram(program.programID);
@@ -66,23 +70,90 @@ int main(int argc, char *argv[])
         program.setProjectionMatrix(projectionMatrix);
         program.setViewMatrix(viewMatrix);
         
-        float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+        //binding ship1 texture
         
-        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+        float ticks = (float) SDL_GetTicks()/1000.0f;
+        
+        float elapsed = ticks - lastFrameTicks;
+        
+        lastFrameTicks = ticks;
+        
+        modelMatrix.identity();
+        
+        modelMatrix.Translate(0,lastFrameTicks,0);
+        
+        program.setModelMatrix(modelMatrix);
+        
+        glBindTexture(GL_TEXTURE_2D, shipTexture);
+        
+        float ship1Vert[] = {-2.5, -0.5, -1.5, -0.5, -1.5, 0.5, -2.5, -0.5, -1.5, 0.5, -2.5, 0.5};
+        
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ship1Vert);
         
         glEnableVertexAttribArray(program.positionAttribute);
         
-        float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        float ship1Text[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
         
-        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, ship1Text);
         
-        glBindTexture(GL_TEXTURE_2D, shipTexture);
+        glEnableVertexAttribArray(program.texCoordAttribute);
         
         glDrawArrays(GL_TRIANGLES, 0, 6);
         
         glDisableVertexAttribArray(program.positionAttribute);
         
         glDisableVertexAttribArray(program.texCoordAttribute);
+        
+        //binding ship2 texture
+        
+        modelMatrix.identity();
+        
+        program.setModelMatrix(modelMatrix);
+        
+        glBindTexture(GL_TEXTURE_2D, shipTexture2);
+        
+        float ship2Vert[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+        
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ship2Vert);
+        
+        glEnableVertexAttribArray(program.positionAttribute);
+        
+        float ship2Text[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, ship2Text);
+        
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        
+        glDisableVertexAttribArray(program.texCoordAttribute);
+        
+        //binding ship3 texture
+        
+        glBindTexture(GL_TEXTURE_2D, shipTexture3);
+        
+        float ship3Vert[] = {1.5, -0.5, 2.5, -0.5, 2.5, 0.5, 1.5, -0.5, 2.5, 0.5, 1.5, 0.5};
+        
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ship3Vert);
+        
+        glEnableVertexAttribArray(program.positionAttribute);
+        
+        float ship3Text[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, ship3Text);
+        
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        
+        glDisableVertexAttribArray(program.texCoordAttribute);
+        
+
+        
 
         SDL_GL_SwapWindow(displayWindow);
     }
